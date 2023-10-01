1da8c4d5f14f339cc76ea7a4bfaafe67d029447a
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-10-01 23:48:42 +0300

    dz

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImageUrl = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600" style="border-radius: 10px;" >
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
